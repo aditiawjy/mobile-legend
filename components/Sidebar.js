e3a2dc10c39ba5f37d278693c2a7b13e5f7ca381
Adit <rva.ant@gmail.com>
2025-11-19 19:28:01 +0700

    apa aja1

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -221,11 +221,7 @@ export default function Sidebar({ children }) {
         </div>
 
         <main className="flex-1 relative overflow-y-auto focus:outline-none">
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              {children}
-            </div>
-          </div>
+          {children}
         </main>
       </div>
     </div>
